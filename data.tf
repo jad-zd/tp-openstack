d8fb4cd7e4c0625f334d6e22b5b49e15eb10ed42
Zahreddine Jad <jad.zahreddine@student-cs.fr>
2023-01-17 22:20:19 +0100

    Feature: import the 2 networks, 2 subnets and cirros VM created with OpenStack CLI initially

--- data.tf
+++ data.tf
@@ -21,3 +21,26 @@ data "openstack_images_image_v2" "cirros" {
 data "openstack_networking_router_v2" "routerstonks" {
 	name = "routerstonks"
 }
+
+# Import the net6stonks network from the OpenStack cloud
+data "openstack_networking_network_v2" "net6stonks" {
+  name = "net6stonks"
+}
+
+data "openstack_networking_subnet_v2" "subnet6stonks" {
+  name = "subnet6stonks"
+}
+
+# Import the initial network from the OpenStack cloud
+data "openstack_networking_network_v2" "netstonks" {
+  name = "netstonks"
+}
+
+data "openstack_networking_subnet_v2" "subnetstonks" {
+  name = "subnetstonks"
+}
+
+# Import the initial cirros VM from the OpenStack cloud
+data "openstack_compute_instance_v2" "vmstonks" {
+  id = "b0dd8dc1-2900-4fa2-9f88-10f0aa7f8273"
+}
