6691024a7ce2ad7b5ba0e84c3ec2bfeaa1758e2b
Zahreddine Jad <jad.zahreddine@student-cs.fr>
2023-01-17 18:04:13 +0100

    Feature: import the available images and flavors

---
+++ data.tf
@@ -0,0 +1,18 @@
+# Import the available flavors and images from the OpenStack cloud
+data "openstack_compute_flavor_v2" "small" {
+	name = "m1.small"
+}
+
+data "openstack_compute_flavor_v2" "tiny" {
+	name = "m1.tiny"
+}
+
+data "openstack_images_image_v2" "debian" {
+  name       = "debian"
+  visibility = "public"
+}
+
+data "openstack_images_image_v2" "cirros" {
+  name       = "cirros"
+  visibility = "public"
+}
